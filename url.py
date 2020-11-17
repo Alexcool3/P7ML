37cca3fbc66adf8a8f12dfda6261ced3a78ea843
Alexcool3 <36537085+Alexcool3@users.noreply.github.com>
2020-11-17 11:25:08 +0100

    url & web_connection python files added.

---
+++ url.py
@@ -0,0 +1,14 @@
+# This file contains all URLs needed for this project.
+
+from enum import Enum
+
+#  URLs:
+#  "https://p7server.000webhostapp.com/download_files.php"
+
+
+class URL(Enum):
+    SEND = ""
+    DOWNLOAD = "https://p7server.000webhostapp.com/download_files.php"
+
+
+
